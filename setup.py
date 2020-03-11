90ae03cfc4e5eedbdbfe9f94799d4b494bccee33
baschno <bastian.schnorbus@gmail.com>
2020-03-11 19:45:55 +0100

    bumping version to 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyvdr',
-      version='0.1.2',
+      version='0.2.0',
       description='Python library for accessing a Linux VDR via SVDRP',
       url='http://github.com/baschno/pyvdr',
       author='baschno',
