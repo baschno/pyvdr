67e9e2f398ccb9ea6396a13a4fdf81dd2c44fa85
baschno <bastian.schnorbus@gmail.com>
2021-03-17 18:01:00 +0100

    bumping version to 0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='pyvdr',
-      version='0.2.3',
+      version='0.3.0',
       description='Python library for accessing a Linux VDR via SVDRP',
       long_description=long_description,
       long_description_content_type="text/markdown",
